a56f71ea83bd1dcade6e09b5afe360a83cab34ce
Matt Mandelbaum <mattrmandelbaum@gmail.com>
2022-10-07 22:50:35 -0400

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import { Image } from "react-bootstrap";
+
+function Home() {
+    return <div>
+            <Image src="https://tse4.mm.bing.net/th?id=OIP.xG2OSpg5HhDPs9Daj7mZEwHaEQ&pid=Api"/>
+    </div>
+}
+
+export default Home;
