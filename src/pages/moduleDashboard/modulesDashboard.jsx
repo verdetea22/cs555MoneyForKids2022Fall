08ef9a0920f183bfbbbe1f70ec724fec2b048f27
verdetea22 <mikayla.mount22@gmail.com>
2022-10-16 19:39:02 -0400

    added three columns and modules header

--- src/pages/moduleDashboard/modulesDashboard.jsx
+++ src/pages/moduleDashboard/modulesDashboard.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import "modules.css";
 
 function ModulesDashboard() {
-  return <div> This is a virtual piggybank app </div>;
+  return (
+    <div>
+      <br />
+      <h2>Learning Modules</h2>
+      <br />
+      <div class="row">
+        {/* Column 1 */}
+        <div class="column">
+          <div id="box"></div>
+        </div>
+
+        {/* Column 2 */}
+        <div class="column"> </div>
+        <div id="box"></div>
+        {/* Column 3 */}
+
+        <div class="column"> </div>
+        <div id="box"></div>
+      </div>
+    </div>
+  );
 }
 
 export default ModulesDashboard;
