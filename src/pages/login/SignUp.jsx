713cb34aab432e91c57dc0379781687a9853b25d
verdetea22 <mikayla.mount22@gmail.com>
2022-10-16 14:36:33 -0400

    start of create account

--- src/pages/login/SignUp.jsx
+++ src/pages/login/SignUp.jsx
@@ -4,11 +4,43 @@ import "./login.css";
 
 function SignUp() {
   return (
-    <div id="box">
-      <a id="text"> Are you a returning user? </a>
-      <a href="/Login" target="_blank">
-        <Button id="button"> Login </Button>{" "}
-      </a>
+    <div>
+      <div id="box">
+        <a id="text"> Are you a returning user? </a>
+        <a href="/Login" target="_blank">
+          <Button id="button"> Login </Button>{" "}
+        </a>
+      </div>
+
+      <div id="box">
+        <a id="header">
+          <br />
+          <h2> Create an Account </h2>
+          <br />
+          <form>
+            {/* Name */}
+            <a id="text">Name:</a>
+            <input id="input" type="Name" />
+
+            {/* Age */}
+            <a id="text">Age:</a>
+            <input id="input" type="Age" />
+            <br />
+
+            {/* Caregiver Email */}
+            <a id="text">Caregiver's Email:</a>
+            <input id="input" type="Email" />
+
+            {/* Username */}
+            <a id="text">Username:</a>
+            <input id="input" type="Username" />
+
+            {/* Password */}
+            <a id="text">Password:</a>
+            <input id="input" type="Password" />
+          </form>
+        </a>
+      </div>
     </div>
   );
 }
