39f12f86b7c23e2d91a52293bc192eeb5554102e
Belle Roberts <belleroberts13@gmail.com>
2022-10-16 19:36:51 -0400

    Added Content

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+
 import { Image } from "react-bootstrap";
 
-function About() {
-    return <div>This is a virtual piggybank app</div>
-    
+function Home() {
+    return <div>
+            <h1>About Us</h1>
+            <div>
+                <h2>A Better Way to Imagine Allowance</h2>
+                <p>Money4Kids was inspired by the idea to make managing money more accessabile to kids. Our goal is to make sure children understand
+                    how to manage their own bank account and funds when they get one. To do this, we simulate a real life bank account, but one that was managed at
+                    home and with you, their parent or guardian. 
+
+                    As a parent, you will be able to create an account for yourself and for your children. From there, the parent account will be able to add and deduct
+                    funds from the children's accounts. Kids will be able to do chores for allowance or deposit their gift money with you, the parent, and have it reflect in their
+                    own accounts to withdraw when needed or save up for something they want. 
+                </p>
+            </div>
+    </div>
 }
 
-export default About;
+export default Home;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
             <div>
                 <h2>Get Started With a Virtual Piggy Bank Today</h2>
                 <p>Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
+                <button href="/SignUp">Create an Account</button>
             </div>
     </div>
 }
