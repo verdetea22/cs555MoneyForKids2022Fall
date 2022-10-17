4a2c54e31febc2946b0c97b480336c7f2eb7b404
Belle Roberts <belleroberts13@gmail.com>
2022-10-16 21:00:25 -0400

    Styling of Home and About pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-
+import "./About.css"
 import { Image } from "react-bootstrap";
 
-function Home() {
-    return <div>
-            <h1 id="about-title">About Us</h1>
-            <div>
-                <h2 id="about-subtitle">A Better Way to Imagine Allowance</h2>
-                <p id="about-body">Money4Kids was inspired by the idea to make managing money more accessabile to kids. Our goal is to make sure children understand
-                    how to manage their own bank account and funds when they get one. To do this, we simulate a real life bank account, but one that was managed at
-                    home and with you, their parent or guardian. 
+function About() {
+    return <div class="splash">
+        <h1 id="about-title" class="splash-head">About Us</h1>
+            <h2 id="about-subtitle" class="splash-subhead">A Better Way to Imagine Allowance</h2>
+            <p id="about-body" class="body">
+                Money4Kids was inspired by the idea to make managing money more accessabile to kids. Our goal is to make sure children understand
+                how to manage their own bank account and funds when they get one. To do this, we simulate a real life bank account, but one that was managed at
+                home and with you, their parent or guardian. 
 
-                    As a parent, you will be able to create an account for yourself and for your children. From there, the parent account will be able to add and deduct
-                    funds from the children's accounts. Kids will be able to do chores for allowance or deposit their gift money with you, the parent, and have it reflect in their
-                    own accounts to withdraw when needed or save up for something they want. 
-                </p>
-            </div>
+                As a parent, you will be able to create an account for yourself and for your children. From there, the parent account will be able to add and deduct
+                funds from the children's accounts. Kids will be able to do chores for allowance or deposit their gift money with you, the parent, and have it reflect in their
+                own accounts to withdraw when needed or save up for something they want. 
+            </p>
     </div>
 }
 
-export default Home;
+export default About;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,17 +5,14 @@ import piggybank from "../../images/piggybank.jpeg"
 
 function Home() {
     return <div>
-            <div>
-                <img src={piggybank} alt="piggybank" id="home-image"/>
-            </div>
-            <div class="splash">
-                <h1 id="Home-Header" class="splash-head">Teach Your Child How to Manage Money</h1>
+        <div class="splash">
+            <h1 id="Home-Header" class="splash-head">Teach Your Child How to Manage Money</h1>
 
-                <h2 id="Home-subtitle" class="splash-subhead">Get Started With a Virtual Piggy Bank Today</h2>
-                <p>Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
-                <button id="Home-Redirect" href="/signUp" class="pure-button pure-button-primary">Create an Account</button>     
-            </div>
-        </div>  
+            <h2 id="Home-subtitle" class="splash-subhead">Get Started With a Virtual Piggy Bank Today</h2>
+            <p class="body">Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
+            <a id="Home-Redirect" href="/SignUp" class="pure-button pure-button-primary">Create an Account</a>     
+        </div>
+    </div>  
 }
 
 export default Home;
