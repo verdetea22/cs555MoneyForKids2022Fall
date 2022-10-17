37d5b36704a987ed5788c4a7ca1464e889e456d9
jcorrido <jcorrido@stevens.edu>
2022-10-16 22:02:58 -0400

    added balance

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,12 +16,6 @@ function Home() {
             <p>Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
             <button id="Home-Redirect" href="/signUp" class="pure-button pure-button-primary">Create an Account</button>
         </div>
-        <div class="card">
-            <h5 class="card-header">Balance</h5>
-            <div class="card-body">
-                <p class="card-text">$1800.00</p>
-            </div>
-        </div>
     </div>  
 }
 
