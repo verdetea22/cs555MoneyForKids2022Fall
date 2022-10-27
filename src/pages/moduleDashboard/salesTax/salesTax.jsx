89fd2c35eafb221225ebba78fcecfa6cb33aa357
verdetea22 <mikayla.mount22@gmail.com>
2022-10-27 19:09:17 -0400

    skeleton of sales tax is solid, will use for other modules

--- src/pages/moduleDashboard/salesTax/salesTax.jsx
+++ src/pages/moduleDashboard/salesTax/salesTax.jsx
@@ -17,9 +17,10 @@ function SalesTaxMod() {
       <div id="sales_tax_description_box">
         <a id="sales_tax_description">
           <br />
-          One way that governments raise money is through sales tax. A sales tax
-          is a fee that is added to the price of an item. When you buy an item
-          that is taxed, you pay the cost of the item plus the sales tax.
+          One way that governments raise money is through sales tax. <br />
+          <br />A sales tax is a fee that is added to the price of an item. When
+          you buy an item that is taxed, you pay the cost of the item plus the
+          sales tax.
           <br />
           <br />
           Click the button to learn more!
