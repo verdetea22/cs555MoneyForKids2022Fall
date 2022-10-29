720361c481942ab3ad491732a5efeb3629f0cb55
Dylan James <djjames455@gmail.com>
2022-10-29 14:56:20 -0400

    Add function to get user data when signed in

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,15 +4,19 @@ import { Image } from "react-bootstrap";
 import piggybank from "../../images/piggybank.jpeg"
 
 function Home() {
-    return <div>
-        <div class="splash">
-            <h1 id="Home-Header" class="splash-head">Teach Your Child How to Manage Money</h1>
 
-            <h2 id="Home-subtitle" class="splash-subhead">Get Started With a Virtual Piggy Bank Today</h2>
-            <p class="body">Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
-            <a id="Home-Redirect" href="/SignUp" class="pure-button pure-button-primary">Create an Account</a>     
+
+    
+    
+    return (<div>
+        <div className="splash">
+            <h1 id="Home-Header" className="splash-head">Teach Your Child How to Manage Money</h1>
+
+            <h2 id="Home-subtitle" className="splash-subhead">Get Started With a Virtual Piggy Bank Today</h2>
+            <p className="body">Money4Kids is a way for parents to teach their children how to manage money in an interactive way where everyone is involved!</p>
+            <a id="Home-Redirect" href="/SignUp" className="pure-button pure-button-primary">Create an Account</a>     
         </div>
-    </div>  
+    </div>);
 }
 
 export default Home;
--- src/services/firebase/db.js
+++ src/services/firebase/db.js
@@ -1,5 +1,6 @@
-import { db } from "./firebase-config";
-import { setDoc, doc } from "firebase/firestore";
+import { db, auth } from "./firebase-config";
+import { setDoc, doc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const setUser = async ({ name, email, username, uid }) => {
     const userRef = doc(db, "users", uid);
@@ -7,4 +8,21 @@ const setUser = async ({ name, email, username, uid }) => {
     await setDoc(userRef, { name, email, username });
 }
 
-export { setUser };
+const getUser = async () => {
+    return new Promise((resolve, reject) => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user && user.uid) {
+                const userRef = doc(db, "users", user.uid);
+                const docSnap = await getDoc(userRef);
+
+                if (docSnap.exists()) {
+                    resolve(docSnap.data());
+                }
+            } else {
+                reject(new Error("getUser - User not found!"));
+            }
+        });
+    });
+}
+
+export { setUser, getUser };
