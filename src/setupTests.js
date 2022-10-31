d16d1e325e567d2e1b45cf6e618e44b662349473
Dylan James <djjames455@gmail.com>
2022-10-30 23:12:52 -0400

    Create test cases

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,21 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Login from "./pages/login/Login";
+import Dashboard from "./pages/ParentPages/Dashboard";
+
+describe("Login", () => {
+    test("renders Login component", () => {
+        render(<Login />);
+
+        screen.debug();
+    });
+});
+
+describe("Dashboard", () => {
+    test("renders Dashboard component", () => {
+        render(<Dashboard />);
+
+        screen.debug();
+    });
+});
