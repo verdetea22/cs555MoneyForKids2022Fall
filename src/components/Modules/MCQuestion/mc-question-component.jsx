fabdd6eaf1310dbeba3dc909ecb54646658e8e1b
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-10-30 22:12:12 -0400

    connected dashboard

--- src/components/Modules/MCQuestion/mc-question-component.jsx
+++ src/components/Modules/MCQuestion/mc-question-component.jsx
@@ -8,9 +8,11 @@ const MCQuestionComponent = (props) => {
         return answer === props.question.correctAnswer;
     }
 
-    return  <Stack className='col-md-8 mx-auto' gap={3} direction='vertical'>
-                <Row className="image-div" md={"auto"}>
+    return  <Stack className='col-md-100 mx-auto' gap={3} direction='vertical'>
+                <Row>
                     <h1>{props.question.question}</h1>
+                </Row>
+                <Row className="image-div" md="auto">
                     {props.question.imageSrc === "" ? null : <Image src={require("../../../assets/modules/"+props.question.imageSrc)}/>}
                 </Row>
                 <Row className="answers-div">
