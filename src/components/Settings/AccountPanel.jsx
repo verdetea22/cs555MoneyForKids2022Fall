535fbd80211a4fc23a3bc3c1a4d77ba65518feac
Dylan James <djjames455@gmail.com>
2022-11-05 20:38:36 -0400

    Create change password related components

---
+++ src/components/Settings/AccountPanel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
+
+function AccountPanel() {
+    const directToChangePasswordPage = () => {
+        window.location.href = "/ChangePassword";
+    } 
+
+    return (
+        <Container>
+            <Row>
+                <div>
+                    <h1 className="border-dark border-bottom border-5 py-2">Change Password</h1>
+                    <Button onClick={directToChangePasswordPage}>Change username</Button>
+                </div>
+            </Row>
+        </Container>
+    );
+}
+
+export default AccountPanel;
--- src/pages/settings/AccountSettings.jsx
+++ src/pages/settings/AccountSettings.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
+import React, { useState } from "react";
+
+import { Container, Nav, Row, Col } from "react-bootstrap";
+import AccountPanel from "../../components/Settings/AccountPanel";
 
 function AccountSettings() {
+
+    const [panel, setPanel] = useState(<></>);
+
+    const handleSelect = (eventKey) => {
+        if (eventKey === "children") {
+
+        } else if (eventKey === "account") {
+            setPanel(<AccountPanel />)
+        }
+    };
+
     return (
-        <><div>
-            <Nav defaultActiveKey="" className="flex-column">
-                <Nav.Link>Children</Nav.Link>
-                <Nav.Link>Account</Nav.Link>
-                <Nav.Link>Link 1</Nav.Link>
-                <Nav.Link>Link 1</Nav.Link>
-            </Nav>
-            </div></>
+        <Container>
+            <Row>
+                <Col>
+                <Nav defaultActiveKey="" className="flex-column" onSelect={handleSelect}>
+                    <Nav.Link eventKey="account">Account</Nav.Link>
+                    <Nav.Link eventKey="children">Children</Nav.Link>
+                    <Nav.Link>Link 1</Nav.Link>
+                    <Nav.Link>Link 1</Nav.Link>
+                </Nav>
+                </Col>
+                <Col>
+                    {panel}
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
