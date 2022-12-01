c9edc5e6def44505926d5b53cc4ca6b04be55711
mmandelb2201 <mattrmandelbaum@gmail.com>
2022-12-01 18:19:11 -0500

    refactored module dashboard

---
+++ src/components/Modules/Common/ModulePreview.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+
+const ModulePreview = ({title, description, link}) => {
+
+    return <Card style={{ width: '18rem', margin: '1rem' }}>
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>{description}</Card.Text>
+                    </Card.Body>
+                    <Button variant="primary" style={{marginBottom: '0.5rem'}} onClick={() => window.location.href = "modules/module/" + link}>Go!</Button>
+                </Card>
+}
+
+export default ModulePreview;
